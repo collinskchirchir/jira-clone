23bfee54a234ded99e1a376d03c226ec7a76df9e
Collins Chirchir <collinskchirchir@gmail.com>
2025-04-27 17:46:06 +0300

    extract hono app to own file

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,8 +1,7 @@
-import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
 import auth from '@/features/auth/server/route';
 
-const app = new Hono().basePath("/api")
+import app from './app';
 
 const routes = app
   .route("/auth", auth)
