dc193974e0f022a4db6adda45b7fb7889ed61fa1
Collins Chirchir <collinskchirchir@gmail.com>
2025-04-27 15:29:42 +0300

    tanstack query setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import { cn } from '@/lib/utils';
+import { QueryProvider } from '@/components/query-provider';
+
 import './globals.css';
 
 const geistSans = localFont({
@@ -28,9 +30,11 @@ export default function RootLayout({
   return (
     <html lang="en">
     <body
-      className={cn(geistSans.variable, geistMono.variable, "antialiased min-h-screen")}
+      className={cn(geistSans.variable, geistMono.variable, 'antialiased min-h-screen')}
     >
-    {children}
+    <QueryProvider>
+      {children}
+    </QueryProvider>
     </body>
     </html>
   );
