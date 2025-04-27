de629d654ac89c94ce739118c6800285f84620ea
Collins Chirchir <collinskchirchir@gmail.com>
2025-04-27 18:41:49 +0300

    onError hono middleware handler

--- src/app/api/[[...route]]/app.ts
+++ src/app/api/[[...route]]/app.ts
@@ -1,8 +1,14 @@
 import { Hono } from 'hono';
 import notFound from '@/middlewares/not-found';
+import onError from '@/middlewares/on-error';
 
 const app = new Hono().basePath("/api")
 
+app.get("/error", (c) => {
+  throw new Error("Oh No, Error!!")
+})
+
 app.notFound(notFound)
+app.onError(onError)
 
 export default app
