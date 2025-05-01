8ae1f1a24b529b4dc2ae7134ad7734f47c45900e
Collins Chirchir <collinskchirchir@gmail.com>
2025-05-01 18:23:24 +0300

    useCurrent hook to check currently logged in user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import { Button } from '@/components/ui/button';
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useCurrent } from '@/features/auth/api/use-current';
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push('/sign-in');
+    }
+  });
   return (
     <div>
-      <Button variant="destructive" size={'sm'}>Click Me!</Button>
+      Only visible to authorized users.
     </div>
   );
 }
