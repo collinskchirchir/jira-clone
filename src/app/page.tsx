af151e385f5cdc80da662464420de93ff28467f5
Collins Chirchir <collinskchirchir@gmail.com>
2025-05-01 19:37:24 +0300

    useLogout hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 'use client';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+
+import { useLogout } from '@/features/auth/api/use-logout';
 import { useCurrent } from '@/features/auth/api/use-current';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   const router = useRouter();
   const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
   useEffect(() => {
     if (!data && !isLoading) {
       router.push('/sign-in');
@@ -14,6 +18,9 @@ export default function Home() {
   return (
     <div>
       Only visible to authorized users.
+      <Button onClick={() => mutate()}>
+        Logout
+      </Button>
     </div>
   );
 }
