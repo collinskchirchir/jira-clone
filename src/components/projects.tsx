188fa113259f33fdd0d9d497d079d1f2f16b2269
Collins Chirchir <collinskchirchir@gmail.com>
2025-05-29 22:58:41 +0300

    project avatar

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -8,6 +8,7 @@ import { useGetProjects } from '@/features/projects/api/use-get-projects';
 import { useWorkspaceId } from '@/features/workspaces/hooks/use-workspace-id';
 import { cn } from '@/lib/utils';
 import { useCreateProjectModal } from '@/features/projects/hooks/use-create-project-modal';
+import ProjectAvatar from '@/features/projects/components/project-avatar';
 
 export const Projects = () => {
   const projectId = null; // TODO: Use the UseProjectId hook
@@ -32,6 +33,7 @@ export const Projects = () => {
               'flex items-center gap-2.5 p-2.5 rounded-md hover:opacity-75 transition cursor-pointer text-neutral-500',
               isActive && 'bg-white shadow-sm hover:opacity-100 text-primary',
             )}>
+              <ProjectAvatar image={project.imageUrl} name={project.name} />
               <span className="truncate">{project.name}</span>
             </div>
           </Link>
